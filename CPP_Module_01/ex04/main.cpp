e21922c0def08bc0a58596ff81fc9e8f7214e5fc
Raul Cortes Benitez <rcortes-@car5s5.42barcelona.com>
2024-05-15 15:10:05 +0200

    ex04 done - file handling

--- CPP_Module_01/ex04/main.cpp
+++ CPP_Module_01/ex04/main.cpp
@@ -1,11 +1,48 @@
-#include "file.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+static void	fill_file(std::fstream &old_file, std::ofstream &new_file, std::string s1, std::string s2)
+{
+	std::string	line;
+	size_t		found = 0;
+	int			i = 0, l = 0;
+	int			trigger = 0;
+	int			is_first = 1;
+	while (std::getline(old_file, line))
+	{
+		if (!is_first)
+			new_file << std::endl;
+		is_first = 0;
+		trigger = 0;
+		for (i = 0; line[i]; i++)
+		{
+			found = line.find(s1, i);
+			if (found == std::string::npos)
+				trigger = 1;
+			if (!trigger && found == (size_t)i)
+			{
+				std::cout << "first: " << i << std::endl;
+				i += s1.size() - 1;
+				std::cout << "second: " << i << std::endl;
+				for (l = 0; s2[l]; l++)
+					new_file << s2[l];
+			}
+			else if (line[i])
+				new_file << line[i];
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
 	std::fstream	f;
-	if (argc < 2 || argc > 4)
+	std::string		new_str = "";
+	std::string		file_format = ".replace";
+
+	if (argc < 2 || argc > 4 || !*argv[2])
 	{
-		std::cout << "Wrong number of arguments !" << std::endl;
+		std::cout << "Wrong number of arguments or invalid input!" << std::endl;
 		return (1);
 	}
 	f.open(argv[1], std::fstream::in);
@@ -14,10 +51,8 @@ int main(int argc, char **argv)
 		std::cout << "Error opening the file !" << std::endl;
 		return (1);
 	}
-	/*   TO DO LIST
-	Create a file called argv[1].replace
-	Buscar en cada string una ocurrence de s1 y cambiarla por su similar de s2
-	Handle error if s1 len != s2 len ????
-	*/
+	new_str = argv[1] + file_format;
+	std::ofstream	new_file (new_str);
+	fill_file(f, new_file, (std::string)argv[2], (std::string)argv[3]);
 	return (0);
 }
